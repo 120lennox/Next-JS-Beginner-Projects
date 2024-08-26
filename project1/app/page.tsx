632d6ae9fd3842c413b52a0b58c46dc11f6e5eb5
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-26 15:56:26 +0200

    learnt how to add next metadata

--- project1/app/page.tsx
+++ project1/app/page.tsx
@@ -1,3 +1,12 @@
+import type { Metadata} from "next"
+
+export const metadata: Metadata = {
+  title: 'my next learning process',
+  description: 'This is my next learning process',
+
+}
+
+
 export default function Page(){
   return <h1 className="">Hello world</h1>
 }
