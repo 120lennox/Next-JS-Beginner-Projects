48b8f532f351a838fa5e24498345f7af6e1e49e6
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-26 15:43:43 +0200

    now i know a thing about routings in nextjs

--- project1/app/layout.tsx
+++ project1/app/layout.tsx
@@ -3,3 +3,18 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 
+
+export default function RootLayout({children,}: {
+  children: React.ReactNode;
+}){
+  return (
+    <html lang="en">
+      <body>
+        <div className="w-full bg-blue-950">
+          <h1>layout</h1>
+        </div>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
