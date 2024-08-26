3f7421bd381f9609beca550a671e1cfe90a7c4c1
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-26 15:49:18 +0200

    now something about nested layouts

---
+++ project1/app/dashboard/layout.tsx
@@ -0,0 +1,6 @@
+export default function DashboardLayout({children,}:{children: React.ReactNode}){
+    return <section>
+        <h2>This is dashboard</h2>
+        {children}
+        </section>
+}
