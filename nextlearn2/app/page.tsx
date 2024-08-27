1d5574f8f19c1a458779f4689e26575e8d3d7558
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-27 15:01:39 +0200

    created basic routings. added a navbar with daisy UI and  implemented the routings in it

--- nextlearn2/app/page.tsx
+++ nextlearn2/app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello, world!</h1>
-      <button className="btn">test button</button>
+    <main className="">
+      <h1>NextJS</h1>
+      <div>
+        <ul>
+          
+        </ul>
+      </div>
     </main>
   );
 }
