58ce7a3731e044028a6dff6aa1c5de316084942f
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-28 09:11:09 +0200

    rendering github users. rendering the users on a table

--- nextlearn2/app/gitusers/page.tsx
+++ nextlearn2/app/gitusers/page.tsx
@@ -1,5 +1,50 @@
-export default function GitUserPage(){
+
+async function fetchGitHubUsers(){
+    const res = await fetch("https://api.github.com/search/users?q=greg")
+    const json = await res.json()
+    return json.items
+}
+
+const GitUserPage = async()=>{
+    const users = await fetchGitHubUsers()
+    console.log(users)
     return <div>
-        <h1>Github users here Page</h1>
+        <div className="overflow-x-auto">
+        <table className="table">
+            {/* head */}
+            <thead>
+            <tr>
+                <th>
+                <label>
+                    <input type="checkbox" className="checkbox" />
+                </label>
+                </th>
+                <th>Name</th>
+                <th>URL</th>
+                <th>Repos</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            {/* row 1 */}
+                { users.map((user)=>(
+                    <tr key={user.id}>
+                        <td>
+                            <div className="flex items-center space-x-3 ">
+                                <div className="avatar">
+                                    <div className="mask mask-squircle w-12 h-12">
+                                        <img src={user.avatar_url} alt="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                )) }
+            {/* row 2 */}
+            </tbody>
+        </table>
+        </div>
     </div>
 }
+
+export default GitUserPage
