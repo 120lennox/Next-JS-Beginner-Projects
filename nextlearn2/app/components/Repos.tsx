d420b13873af75cecab118429a49ba4e80e8c9f8
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-28 14:46:01 +0200

    added seperate page for repositories

--- nextlearn2/app/components/Repos.tsx
+++ nextlearn2/app/components/Repos.tsx
@@ -10,7 +10,29 @@ const Repos = async({user}) =>{
     console.log(repos)
 
     return <div>
-        <h1>{user}'s Repos</h1>
+        <div className="mt-5 ml-5">
+            <h1 className="text-gray-300 text-lg">{user}'s Repos</h1>
+            {/* table */}
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Repo Name</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {repos.map((repo)=>(
+                            <tr>
+                                <td>{repo.name}</td>
+                                <td>{repo.description}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
     </div>
 }
 
