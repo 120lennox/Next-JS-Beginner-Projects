a3e25e54d251922d15b37594d3cddc6f3bea350a
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-28 14:58:15 +0200

    added loading component

---
+++ nextlearn2/app/loading.tsx
@@ -0,0 +1,5 @@
+export default function LoadingPage(){
+    return <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-dots loading-md"></span>
+    </div>
+}
