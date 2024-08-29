2108e26743e4dd14c8e1abe0c1bdfe871bf12cab
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-29 14:36:25 +0200

    added cards on home page

--- nextlearn2/app/components/books.tsx
+++ nextlearn2/app/components/books.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 
 async function getBooks(){
     const res = await fetch("http://localhost:3000/api/books", {
@@ -9,15 +11,31 @@ async function getBooks(){
     return json
 }
 
-async function bookView(){
+const Books = async()=>{
     const books = await getBooks()
     return <div>
         <h1>Books here</h1>
-        <div>
+        <div className="flex justify-between items-center">
             {books.map((book)=>(
                 <div key={book.id}>
                     <div className="card w-96 bg-base-100 shadow-xl">
-
+                        <figure>
+                            <img src={book.image} width="200" height="150" alt="" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                {book.id}
+                            </h2>
+                            <p>
+                                {book.title}
+                            </p>
+                            <div className="card-actions justify-end">
+                                <Link href={book.link} className="btn btn-primary"> See in Amazon </Link>
+                                <button className="btn btn-error">
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             ))}
@@ -25,4 +43,4 @@ async function bookView(){
     </div>
 }
 
-export default bookView;
+export default Books;
--- nextlearn2/app/page.tsx
+++ nextlearn2/app/page.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
-import bookView from "./components/books";
+import Books from "./components/books";
+
 
 export default function Home() {
   return (
     <main className="">
       <div className="mt-5 flex justify-center items-center flex-col">
         <h1 className="text-gray-100 text-6xl">Nthano's Portifolio of Small Bets</h1>
-        
+        <div>
+          <Books/>
+        </div>
       </div>
     </main>
   );
