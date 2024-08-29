dc593f4ce51374d4ddfa9c097e3f131f0f00578f
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-29 15:55:14 +0200

    trying to retrieve results from the query

--- nextlearn2/app/api/books/search/route.tsx
+++ nextlearn2/app/api/books/search/route.tsx
@@ -3,6 +3,12 @@ import books from '../data.json'
 
 export async function GET(req){
     const {searchParams} = new URL(req.url)
-    console.log(searchParams)
-    return NextResponse.json(books)
+    const query = searchParams.get('query')
+
+    const filteredResults = books.filter((book)=>{
+        return book.title.toLowerCase().includes(query.toLowerCase())
+    })
+
+    console.log(filteredResults)
+    return NextResponse.json(filteredResults)
 }
