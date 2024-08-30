6e32f9ebe2110dbc35f6579aec647ca01586b33a
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-30 16:30:03 +0200

    will restart later

--- nextlearn2/app/api/books/route.ts
+++ nextlearn2/app/api/books/route.ts
@@ -6,3 +6,16 @@ export async function GET(req){
     return NextResponse.json(books)
 }
 
+export async function POST(req){
+    const {title, link, image } = await req.json()
+
+    const newBook = {
+        id: books.length + 1,
+        title,
+        link,
+        image
+    }
+
+    books.push(newBook)
+    return NextResponse.json('new book added successfully')
+}
